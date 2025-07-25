84afaa4224625cc7f824f2493db2b56d354af602
Viksata <viki1204394@gmail.com>
2025-07-25 16:24:25 +0300

    Last Problem Solved

--- JS Front-End/DOM-Manipulation-and-Events/hightlight-active/hightlight-active.js
+++ JS Front-End/DOM-Manipulation-and-Events/hightlight-active/hightlight-active.js
@@ -1,6 +1,18 @@
 document.addEventListener('DOMContentLoaded', focused);
 
-// TODO: Solve the problem.
 function focused() {
-    
+    const inputEl = document.querySelectorAll('input');
+
+    inputEl.forEach(input => {
+        input.addEventListener('focus', onFocus);
+        input.addEventListener('blur', onBlur);
+
+        function onFocus(e) {
+            e.target.parentElement.classList.add('focused');
+        }
+
+        function onBlur(e) {
+            e.target.parentElement.classList.remove('focused');
+        }
+    })
 }
